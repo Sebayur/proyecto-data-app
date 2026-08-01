10143a511439a2cab164a4332700179ce6769e4d
Sebayur <sebastianyurtgu@gmail.com>
2026-08-01 20:28:55 -0300

    Agrego la creacion de usuario desde la interfaz

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import pandas as pd
 from sqlalchemy import create_engine 
 
@@ -7,7 +7,8 @@ engine = create_engine("sqlite:///database/app.db")
 app = Flask(__name__)
 
 #Cargar datos
-df = pd.read_sql("SELECT * FROM usuarios", engine) # Leé el resultado de la consulta SQL y lo guarda en un DataFrame.
+def obtener_usuarios():
+    return pd.read_sql("SELECT * FROM usuarios", engine) # Leé el resultado de la consulta SQL y lo guarda en un DataFrame.
 print("APP CORRECTA CARGADA")
 @app.route("/")
 def home():
@@ -15,10 +16,41 @@ def home():
 
 @app.route('/data')
 def data():
+    df = obtener_usuarios()
     return jsonify(df.to_dict(orient='records'))
+
+@app.route("/agregar", methods=["POST"])
+def agregar_usuario():
+
+    datos = request.json
+
+    nombre = datos["nombre"]
+    edad = datos["edad"]
+    ciudad = datos["ciudad"]
+
+    df = obtener_usuarios()
+
+    nuevo_id = df["id"].max() + 1
+
+    nuevo = pd.DataFrame([{
+        "id": nuevo_id,
+        "nombre": nombre,
+        "edad": edad,
+        "ciudad": ciudad
+    }])
+
+    nuevo.to_sql(
+        "usuarios",
+        engine,
+        if_exists="append",
+        index=False
+    )
+
+    return {"mensaje": "Usuario agregado correctamente"}
 
 @app.route("/usuarios/<int:id>")
 def get_usuario(id):
+       df = obtener_usuarios()
        usuario = df[df["id"] == id]
 
        if usuario.empty:
